d2d4a4906579a6f4fbd9982e57ff9adb7a7a35f4
Jason C. Bryant <60683599+jcbryant35@users.noreply.github.com>
2021-03-08 22:53:27 -0600

    BreakfastTacoRecipe Component

---
+++ BreakfastTacoRecipe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class BreakfastTacoRecipe extends React.Component {
+ 
+  render() {
+    return (
+      <div>
+        <h1>Breakfast Tacos</h1>
+      </div>
+    )
+  };
+};
