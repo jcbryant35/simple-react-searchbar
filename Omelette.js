51f572eab62429edb2d2b5c642ad501c2a0c0d9c
Jason C. Bryant <60683599+jcbryant35@users.noreply.github.com>
2021-03-08 22:55:20 -0600

    Omelette Component

---
+++ Omelette.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class OmeletteRecipe extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Omelettes</h1>
+      </div>
+    )
+  };
+};
