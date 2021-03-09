acc8b4d71d703ddcb02fdcdc92135ddd9907a39a
Jason C. Bryant <60683599+jcbryant35@users.noreply.github.com>
2021-03-08 22:54:24 -0600

    BreakfastBurritoRecipe Component

---
+++ BreakfastBurritoRecipe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class BreakfastBurritoRecipe extends React.Component {
+    
+  render() {
+    return (
+      <div>
+        <h1>Breakfast Burritos</h1>
+      </div>
+    )
+  };
+};
